c3b8336cf1cc914669c2ab05f31fdbf5f8baf4fa
Antoine <a.pacot72@gmail.com>
2021-05-18 10:31:05 +0200

    TestWebSocket

--- Projet2021/Test_WEBSocket/WebSocketServer.ino.cpp
+++ Projet2021/Test_WEBSocket/WebSocketServer.ino.cpp
@@ -2,11 +2,13 @@
 #include <WiFi.h>
 #include <WebServer.h>
 #include <WebSocketsServer.h>
-#include <HardwareSerial.h>
 
 const char *ssid = "BallonSondeAP";
 const char *password = "BallonSonde2021";
 
 WebServer server;
 WebSocketsServer webSocket = WebSocketsServer(81);
+
+WiFi.softAPConfig(IPAddress(192, 168, 5, 1), IPAddress(192, 168, 5, 1),IPAddress(255, 255, 255, 0));
+WiFi.softAP(ssid,password);
 
